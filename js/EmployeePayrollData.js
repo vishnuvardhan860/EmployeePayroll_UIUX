1d194c4e93bf2df98ed05090d392797e85805585
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-09-02 20:35:24 +0530

    Added: Adding To Local Storage

--- js/EmployeePayrollData.js
+++ js/EmployeePayrollData.js
@@ -22,15 +22,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
         output.textContent = salary.value
     });
 });
-
+/*UC3*/
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (error) {
         return;
     }
 }
 
+//Uc4
+function createAndUpdateStorage(employeePayrollData) {
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList!=undefined)
+        employeePayrollList.push(employeePayrollData);
+    else {
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());    
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
+}
+//
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
     try {

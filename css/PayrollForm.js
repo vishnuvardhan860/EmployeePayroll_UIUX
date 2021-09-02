6a8e17a9136eacafbfdadc0ddb9b2d272977f294
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-09-02 20:07:20 +0530

    Added:Adding Salary Event

--- css/PayrollForm.js
+++ css/PayrollForm.js
@@ -1,8 +1,8 @@
-//UC7 - Set Button Style
+//UC8 - Set Range For Salary Output
 
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
-output.textContent = salary.nodeValue;
-salary.addEventListener('input',function(){
-    output.textContent = salary.nodeValue
+output.textContent = salary.value;
+salary.addEventListener('input', function() {
+    output.textContent = salary.value;
 });

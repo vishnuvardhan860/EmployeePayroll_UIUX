aed3b05556718f4faa20ca6b929f6b875112b043
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-09-02 20:22:31 +0530

    Added:Adding Attributes And Getters Setters

--- js/Payroll_form.js
+++ js/Payroll_form.js
@@ -1,75 +1,56 @@
-//UC8 - Set Range For Salary Output
+class EmployeePayrollData {
 
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function() {
-    output.textContent = salary.value;
-});
-
-// UC9
-class EmployeePayRollData {
+    //getter and setter method
+    get id() { return this._id; }
+    set id(id) {
+         this._id = id; 
+        }
 
     get name() { return this._name; }
     set name(name) {
-        this._name = name;
+        let nameRegex = RegExp('^[A-Z][a-z A-Z \\s]{2,}$')
+        if (nameRegex.test(name))
+            this._name = name;
+        else throw 'Name is Incorrect!';
     }
     get profilePic() { return this._profilePic; }
-    set profilePic(profilePic) { this._profilePic = profilePic; }
+    set profilePic(profilePic) {
+         this._profilePic = profilePic 
+        }
 
-    get salary() { return this._salary; }
-    set salary(salary) {
-        this._salary = salary;
-    }
     get gender() { return this._gender; }
     set gender(gender) {
-        this._gender = gender;
-    }
+         this._gender = gender; 
+        }
+
     get department() { return this._department; }
-    set department(department) { this._department = department; }
+    set department(department) {
+         this_department = department; 
+        }
+
+    get salary() { return this._salary; }
+    set salary(salary) { 
+        return this._salary = salary;
+     }
+
+    get note() { return this._note; }
+    set note(note) {
+         this._note = note; 
+        }
 
     get startDate() { return this._startDate; }
     set startDate(startDate) {
-        this._startDate = startDate;
-    }
+         this._startDate = startDate 
+        }
 
-    get notes() { return this._notes; }
-    set notes(notes) {
-        this._notes = notes;
-    }
-
+    //method
     toString() {
-        const options = {
-            day: 'numeric', month: 'numeric', year: 'numeric'
-        };
-        const empDate = !this.startDate ? "undefined" : this.startDate.toLocaleDateString("en-IN", options);
-        return "name = " + this.name + ", profilePic = " + this.profilePic + ", salary = " + this.salary + ", gender = " + this.gender + ", department = " + this.department + ", startDate = " + empDate+ ", notes= " + this.notes;
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const empDate = !this.startDate ? "undefined" :
+            this.startDate.toLocalDateString("en-US", options);
+        return "id=" + this.id + ", name = '" + this.name + ", gender = '" + this.gender +
+            ", profilePic = '" + this.profilePic + ", department = " + this.department +
+            ", Salary = " + this.salary + "startDate = " + empDate + ", note = " + this.note;
+
     }
-}
-
-function save() {
-    let employeePayrollData = new EmployeePayRollData();
-    employeePayrollData.name = getInputValueById("#name");
-    employeePayrollData.profilePic = getSelectedValues("[name = profile]").pop();
-    employeePayrollData.gender = getSelectedValues("[name = gender]").pop();
-    employeePayrollData.department = getSelectedValues("[name = department]");
-    employeePayrollData.salary = getInputValueById("#salary");
-    employeePayrollData.notes = getInputValueById("#notes");
-   employeePayrollData.startDate = new Date(getInputValueById("#year"), getInputValueById("#month"), getInputValueById("#day"));
-   alert(employeePayrollData.toString());
-}
-
-let getSelectedValues = (property) => {
-    let allItems = document.querySelectorAll(property);
-    let selectedItems = [];
-    allItems.forEach(item => {
-        if (item.checked)
-            selectedItems.push(item.value);
-    });
-    return selectedItems;
-}
-
-let getInputValueById = (id) => {
-    let value = document.querySelector(id).value;
-    return value; 
 }
